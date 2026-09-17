a0c2d509a32429bfd09ccc8ed499953011ab6732
bntxperses <dbikramyt@gmail.com>
2026-09-17 16:36:31 +0545

    sm8250-common: Don't override RAM-based read_ahead_kb

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -6192,17 +6192,6 @@ misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
 
-# Runtime fs tuning
-echo 128 > /sys/block/sda/queue/read_ahead_kb
-echo 128 > /sys/block/sda/queue/nr_requests
-echo 1 > /sys/block/sda/queue/iostats
-echo 128 > /sys/block/dm-0/queue/read_ahead_kb
-echo 128 > /sys/block/dm-1/queue/read_ahead_kb
-echo 128 > /sys/block/dm-2/queue/read_ahead_kb
-echo 128 > /sys/block/dm-3/queue/read_ahead_kb
-echo 128 > /sys/block/dm-4/queue/read_ahead_kb
-echo 128 > /sys/block/dm-5/queue/read_ahead_kb
-echo 128 > /sys/block/dm-6/queue/read_ahead_kb
-echo 128 > /sys/block/dm-7/queue/read_ahead_kb
-echo 128 > /sys/block/dm-8/queue/read_ahead_kb
-echo 128 > /sys/block/dm-9/queue/read_ahead_kb
+# Do not override target-specific read-ahead values here.
+# configure_read_ahead_kb_values() already selects values based on RAM size.
+# Keep I/O statistics disabled to avoid unnecessary block-layer accounting.
